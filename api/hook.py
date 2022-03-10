9e2cd6ebd37d19b6e8506a68be00f7dacdef23d6
Thanujan <thanujan2000@hotmail.fr>
2022-03-10 18:08:38 +0100

    read all resources

--- api/hook.py
+++ api/hook.py
@@ -1,4 +1,6 @@
 import os
+
+from sympy import EX
 from config import credentials
 import requests
 import re
@@ -42,55 +44,66 @@ def requests_search(name: str) -> list:
 
 
 def requests_movie(movie_id: int):
-    movie_endpoint = (
-        f"{credentials.API_BASE_URL}/movie/{movie_id}?api_key={credentials.API_KEY}"
-    )
-    credential_endpoint = f"{credentials.API_BASE_URL}/movie/{movie_id}/credits?api_key={credentials.API_KEY}"
-    movie = requests.get(movie_endpoint).json()
-    title = movie["title"]
-    budget = movie["budget"]
-    revenue = movie["revenue"]
-    genres = [movie["genres"][i].get("name") for i in range(len(movie["genres"]))]
-    original_language = movie["original_language"]
-    overview = movie["overview"]
-    production_companies = [
-        movie["production_companies"][i].get("name")
-        for i in range(len(movie["production_companies"]))
-    ]
-    release_date = movie["release_date"]
-
-    runtime = movie["runtime"]
-    vote_average = movie["vote_average"]
-    vote_count = movie["vote_count"]
-
-    r = requests.get(credential_endpoint)
-    cast = r.json()["cast"]
     try:
-        actors = [cast[i] for i in range(5)]
-    except IndexError:
-        actors = [cast[i] for i in range(len(cast))]
-    pop_actors = sum([el["popularity"] for el in actors])
-    crew = r.json()["crew"]
+        movie_endpoint = (
+            f"{credentials.API_BASE_URL}/movie/{movie_id}?api_key={credentials.API_KEY}"
+        )
+        credential_endpoint = f"{credentials.API_BASE_URL}/movie/{movie_id}/credits?api_key={credentials.API_KEY}"
+        movie = requests.get(movie_endpoint)
+        if movie.status_code != 200:
+            mess = "the movie with {} does not exist".format(movie_id)
+            raise Exception(mess)
+        movie = movie.json()
+        title = movie["title"]
+        budget = movie["budget"]
+        revenue = movie["revenue"]
+        genres = [movie["genres"][i].get("name") for i in range(len(movie["genres"]))]
+        # original_language = movie["original_language"]
+        overview = movie["overview"]
+        # production_companies = [
+        #     movie["production_companies"][i].get("name")
+        #     for i in range(len(movie["production_companies"]))
+        # ]
+        # release_date = movie["release_date"]
 
-    directors = [crew[i] for i in range(len(crew)) if crew[i].get("job") == "Director"]
-    pop_directors = sum([el["popularity"] for el in directors])
-    producers = [crew[i] for i in range(len(crew)) if crew[i].get("job") == "Producer"]
-    pop_producers = sum([el["popularity"] for el in producers])
-    input = [
-        movie_id,
-        title,
-        budget,
-        genres,
-        revenue,
-        runtime,
-        vote_average,
-        vote_count,
-        pop_actors,
-        pop_directors,
-        pop_producers,
-    ]
-    print(input)
-    return input
+        runtime = movie["runtime"]
+        vote_average = movie["vote_average"]
+        vote_count = movie["vote_count"]
 
+        r = requests.get(credential_endpoint)
+        if r.status_code != 200:
+            mess = "the movie with {} does not exist".format(movie_id)
+            raise Exception(mess)
+        cast = r.json()["cast"]
+        try:
+            actors = [cast[i] for i in range(5)]
+        except IndexError:
+            actors = [cast[i] for i in range(len(cast))]
+        pop_actors = sum([el["popularity"] for el in actors])
+        crew = r.json()["crew"]
 
-requests_movie(557)
+        directors = [
+            crew[i] for i in range(len(crew)) if crew[i].get("job") == "Director"
+        ]
+        pop_directors = sum([el["popularity"] for el in directors])
+        producers = [
+            crew[i] for i in range(len(crew)) if crew[i].get("job") == "Producer"
+        ]
+        pop_producers = sum([el["popularity"] for el in producers])
+        inputs = dict(
+            movie_id=movie_id,
+            title=title,
+            budget=budget,
+            genres=genres,
+            revenue=revenue,
+            runtime=runtime,
+            vote_average=vote_average,
+            vote_count=vote_count,
+            pop_actors=pop_actors,
+            pop_directors=pop_directors,
+            pop_producers=pop_producers,
+        )
+
+        return {"response": inputs}
+    except Exception as err:
+        return {"error": str(err)}
