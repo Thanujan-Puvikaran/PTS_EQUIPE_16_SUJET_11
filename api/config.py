28dc132cdd9dbf7e1b9a4cdd043dd0e29e6d9531
Thanujan <thanujan2000@hotmail.fr>
2022-03-12 13:53:48 +0100

    Final changes and add notebooks as well as dataset

--- api/config.py
+++ api/config.py
@@ -1,15 +1,25 @@
-class raiseError:
-    JSONERROR = "Failed to decode JSON object: Expecting value"
-    JSONMESSAGE = (
-        "You made a mistake, "
-        "if you do not know a particular value "
-        "just give an empty string if it is a string type variable and 0 "
-        "if it is an integer or float type variable"
-    )
-    TYPEERROR = "TypeError"
-    TYPEMESSAGE = "Wrong variable type, check it carefully"
-
-
 class credentials:
     API_KEY = "a7a0813f3d0ae3a4c26bf99545474eae"
     API_BASE_URL = "https://api.themoviedb.org/3"
+
+
+class reduction:
+    AVERAGES = {
+        "budget": 2.86732153e07,
+        "runtime": 1.03610884e02,
+        "pop_actors": 6.29233460e01,
+        "pop_directors": 4.88691329e00,
+        "pop_producer": 6.81374285e00,
+    }
+    VARIANCES = {
+        "budget": 1.63268781e15,
+        "runtime": 8.02568171e02,
+        "pop_actors": 1.27100987e03,
+        "pop_directors": 3.48424234e01,
+        "pop_producer": 1.05868464e02,
+    }
+
+
+class mapping:
+    PREDICTION_MAP = {"0": "SUPER HIT", "1": "HIT", "2": "FLOP"}
+
